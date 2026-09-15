71db21846f215ab95d9153d39da6a7124bfd9de5
owolabisherif <owolabisherif@emqatar.com>
2026-09-15 17:08:24 +0300

    build frontend assets for deployment

--- public/build/assets/chevron-up-B8viLEb2.js
+++
@@ -1,1 +0,0 @@
-import{Gt as e}from"./app-UvLhBzaS.js";var t=e(`ChevronUp`,[[`path`,{d:`m18 15-6-6-6 6`,key:`153udz`}]]);export{t};
--- public/build/assets/moon-CoVKYSxN.js
+++
@@ -1,1 +0,0 @@
-import{Gt as e}from"./app-UvLhBzaS.js";var t=e(`Moon`,[[`path`,{d:`M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z`,key:`a7tn18`}]]);export{t};
